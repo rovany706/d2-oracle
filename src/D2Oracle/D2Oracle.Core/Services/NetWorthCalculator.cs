b0cbd3985771e1ecb2717fa4938ad182809e836c
Ivan Ryazanov <rovany706@gmail.com>
2023-10-12 23:20:53 +0500

    Add aghanims items calculation

--- src/D2Oracle/D2Oracle.Core/Services/NetWorthCalculator.cs
+++ src/D2Oracle/D2Oracle.Core/Services/NetWorthCalculator.cs
@@ -6,6 +6,7 @@ namespace D2Oracle.Core.Services;
 
 public class NetWorthCalculator
 {
+    private const string AghanimsShardName = "item_aghanims_shard";
     private readonly IDotaKnowledgeService dotaKnowledgeService;
 
     public NetWorthCalculator(IDotaKnowledgeService dotaKnowledgeService)
@@ -15,21 +16,55 @@ public class NetWorthCalculator
 
     public uint Calculate(GameState? gameState)
     {
-        if (gameState?.Items is null || gameState.Player is null)
+        if (gameState?.Items is null || gameState.Player is null || gameState.Hero is null)
         {
             return 0;
         }
 
         var items = gameState.Items.MainItems.Union(gameState.Items.StashItems);
         var gold = gameState.Player.Gold;
-        
-        var itemDescriptions = items.Select(GetItemDescription);
+        gold += GetAghanimsShardCost(gameState);
+        gold += GetAghanimsBlessingCost(gameState);
 
-        return gold + (uint) itemDescriptions.Sum(x => x?.Cost ?? 0);
+        var itemDescriptions = items.Select(GetItemDescription);
+        return gold + (uint)itemDescriptions.Sum(x => x?.Cost ?? 0);
     }
 
     private ItemDescription? GetItemDescription(DotaItem item)
     {
-        return dotaKnowledgeService.Items.SingleOrDefault(description => description.Name.Equals(item.Name));
+        return GetItemDescription(item.Name);
+    }
+
+    private ItemDescription? GetItemDescription(string itemName)
+    {
+        return dotaKnowledgeService.Items.SingleOrDefault(description => description.Name.Equals(itemName));
+    }
+
+    private uint GetAghanimsShardCost(GameState gameState)
+    {
+        if (gameState.Hero?.AghanimsShard == false)
+        {
+            return 0;
+        }
+
+        return (uint?)GetItemDescription(AghanimsShardName)?.Cost ?? 0;
+    }
+
+    /// <summary>
+    /// Get aghanims blessing cost if player has buff with no scepter.
+    /// Does not determine if buff was received from Alchemist (4200 gold instead of 5800).  
+    /// </summary>
+    private uint GetAghanimsBlessingCost(GameState gameState)
+    {
+        var hasPlayerAghanimsBlessing = gameState.Hero?.AghanimsScepter == true // has aghanims upgrade
+                                        && gameState.Items?.MainItems.SingleOrDefault(x => // but no scepter
+                                            x.Name == "item_ultimate_scepter") == null;
+
+        if (hasPlayerAghanimsBlessing)
+        {
+            return (uint?)GetItemDescription("item_ultimate_scepter_2")?.Cost ?? 0;
+        }
+
+        return 0;
     }
 }
