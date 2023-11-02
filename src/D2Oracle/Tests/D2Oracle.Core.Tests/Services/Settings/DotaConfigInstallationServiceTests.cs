562b3fc2e35737f27af37a8e1f1b0c8cb2103924
Ivan Ryazanov <rovany706@gmail.com>
2023-11-03 01:52:03 +0500

    Fix filesystem tests

--- src/D2Oracle/Tests/D2Oracle.Core.Tests/Services/Settings/DotaConfigInstallationServiceTests.cs
+++ src/D2Oracle/Tests/D2Oracle.Core.Tests/Services/Settings/DotaConfigInstallationServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using System.IO.Abstractions.TestingHelpers;
+﻿using System.IO.Abstractions;
+using System.IO.Abstractions.TestingHelpers;
 using D2Oracle.Core.Services.Settings;
 
 namespace D2Oracle.Core.Tests.Services.Settings;
@@ -9,82 +10,61 @@ public class DotaConfigInstallationServiceTests
     public void IsCorrectDotaPath_WhenDirectoryNotExist_ReturnFalse()
     {
         var fileSystem = new MockFileSystem();
+        var path = fileSystem.CurrentDirectory().SubDirectory("dota 2 beta").FullName;
 
         var sut = new DotaConfigInstallationService(fileSystem);
-        
-        Assert.That(sut.IsCorrectDotaPath(@"c:\dota 2 beta"), Is.False);
+
+        Assert.That(sut.IsCorrectDotaPath(path), Is.False);
     }
-    
+
     [Test]
     public void IsCorrectDotaPath_WhenDirectoryExist_ReturnTrue()
     {
         var fileSystem = new MockFileSystem();
-        fileSystem.Directory.CreateDirectory(@"c:\dota 2 beta\game\dota\cfg");
+        var path = fileSystem.CurrentDirectory().SubDirectory("dota 2 beta");
+        path.SubDirectory("game", "dota", "cfg").Create();
 
         var sut = new DotaConfigInstallationService(fileSystem);
-        
-        Assert.That(sut.IsCorrectDotaPath(@"c:\dota 2 beta"), Is.True);
+
+        Assert.That(sut.IsCorrectDotaPath(path.FullName), Is.True);
     }
 
     [Test]
     public async Task InstallConfigAsync_WhenConfigFileExists_DoNotRewriteConfigFile()
     {
         const string expected = "test";
-        const string configFilePath = @"c:\dota 2 beta\game\dota\cfg\gamestate_integration\gamestate_integration_d2oracle.cfg";
-        
-        var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
+        var fileSystem = new MockFileSystem();
+        var directory = fileSystem.CurrentDirectory()
+            .SubDirectory("dota 2 beta", "game", "dota", "cfg", "gamestate_integration");
+        directory.Create();
+        var configFile = directory.File("gamestate_integration_d2oracle.cfg");
+
+        await using (var writer = configFile.CreateText())
         {
-            {
-                configFilePath,
-                new MockFileData(expected)
-            }
-        });
+            await writer.WriteAsync(expected);
+        }
 
         var sut = new DotaConfigInstallationService(fileSystem);
 
-        await sut.InstallConfigAsync(@"c:\dota 2 beta");
-        
-        Assert.That(await fileSystem.File.ReadAllTextAsync(configFilePath), Is.EqualTo(expected));
+        await sut.InstallConfigAsync(fileSystem.CurrentDirectory().SubDirectory("dota 2 beta").FullName);
+
+        Assert.That(await fileSystem.File.ReadAllTextAsync(configFile.FullName), Is.EqualTo(expected));
     }
-    
+
     [Test]
     public async Task InstallConfigAsync_WhenConfigFileNotExist_CreateConfigFile()
     {
         var expected = Resources.Resources.GsiFileContent;
-        const string configFilePath = @"c:\dota 2 beta\game\dota\cfg\gamestate_integration\gamestate_integration_d2oracle.cfg";
-        
-        var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
-        {
-            {
-                @"c:\dota 2 beta\game\dota\cfg\",
-                new MockDirectoryData()
-            }
-        });
+        var fileSystem = new MockFileSystem();
+        var directory = fileSystem.CurrentDirectory()
+            .SubDirectory("dota 2 beta", "game", "dota", "cfg");
+        directory.Create();
 
         var sut = new DotaConfigInstallationService(fileSystem);
 
         await sut.InstallConfigAsync(@"c:\dota 2 beta");
-        
-        Assert.That(await fileSystem.File.ReadAllTextAsync(configFilePath), Is.EqualTo(expected));
-    }
-    
-    [Test]
-    public async Task InstallConfigAsync_WhenConfigFileNotExist_CreateConfigDirectory()
-    {
-        const string configFileDirectory = @"c:\dota 2 beta\game\dota\cfg\gamestate_integration";
-        
-        var fileSystem = new MockFileSystem(new Dictionary<string, MockFileData>
-        {
-            {
-                @"c:\dota 2 beta\game\dota\cfg\",
-                new MockDirectoryData()
-            }
-        });
 
-        var sut = new DotaConfigInstallationService(fileSystem);
-
-        await sut.InstallConfigAsync(@"c:\dota 2 beta");
-        
-        Assert.That(fileSystem.Directory.Exists(configFileDirectory), Is.True);
+        var configFile = directory.File("gamestate_integration", "gamestate_integration_d2oracle.cfg");
+        Assert.That(await fileSystem.File.ReadAllTextAsync(configFile.FullName), Is.EqualTo(expected));
     }
 }
