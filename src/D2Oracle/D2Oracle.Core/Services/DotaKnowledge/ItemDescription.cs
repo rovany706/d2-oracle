f08169a4e8bc7a7bdc4d460857fb6241875acd91
Ivan Ryazanov <rovany706@gmail.com>
2023-12-18 20:02:47 +0500

    Update dota items (7.35)

--- src/D2Oracle/D2Oracle.Core/Services/DotaKnowledge/ItemDescription.cs
+++ src/D2Oracle/D2Oracle.Core/Services/DotaKnowledge/ItemDescription.cs
@@ -1,11 +1,6 @@
 ﻿namespace D2Oracle.Core.Services.DotaKnowledge;
 
 public record ItemDescription(
-    int Id,
     string Name,
-    int Cost,
-    int SecretShop,
-    int SideShop,
-    int Recipe,
-    string LocalizedName
+    int Cost
 );
